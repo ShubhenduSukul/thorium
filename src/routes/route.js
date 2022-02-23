b79862636aa178847587ddcfff7aa7b836d0db69
ShubhenduSukul <shubhendu7870608936@mail.com>
2022-02-23 07:02:40 +0530

    Second project

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,8 +6,27 @@ router.get('/students/:name', function(req, res) {
     console.log(studentName)
     res.send(studentName)
 })
-
-
-
+//1 api movies
+router.get('/movies',function(req,res){
+    res.send('["Hacksaw-Ridge","Tomorrow-War","Spider-man",Wrong-turn","Harry-Potter"]')
+});
+//2. api movie by indexid
+router.get('/movies/:movieId',function(req,res){
+    mov=["Hacksaw-Ridge","Tomorrow-War","Spider-man","Wrong-turn","Harry-Potter"]
+    let value =req.params.movieId;
+    if(value>mov.length-1){
+        res.send('"doesnt exists"')
+    }
+    else{
+        res.send(mov[value])
+    }
+})
+//3. fetch movies from array
+router.get('/moviez',function(req,res){
+    res.send([{id: 1,name:'The Shining'},{id: 2,name:'Incendies'},{id: 3,name :'3 idiots'},{
+        id: 4,name:'Hulk'},{id: 5,name:'Thor'
+    }]);
+})
+//4.
 
 module.exports = router;
